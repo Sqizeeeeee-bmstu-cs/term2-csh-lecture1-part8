6c7d9f275214ce52f8e7b417b7a5c0942640fd2c
Vladimir <vladimir.oleynyk057@gmail.com>
2026-03-25 19:47:11 +0300

    first commit

---
+++ Book.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Book
+{
+    private string title;
+    private string author;
+    private int year;
+    private int pageCount;
+
+    public Book(string title, string author, int year, int pageCount)
+    {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.pageCount = pageCount;
+    }
+
+    public Book(string title, string author) : this (title, author, 2026, 0) {}
+
+    public Book() : this ("", "") {}
+
+    public string GetInfo()
+    {
+        return $"book: {title}, author: {author}, year: {year}, pages: {pageCount}";
+    }
+
+    public string GetInfo(bool showPages)
+    {
+        if (showPages)
+        {
+            return GetInfo();
+        }
+        else
+        {
+            return $"book: {title}, author: {author}, year: {year}";
+        }
+    }
+
+    public bool IsOlderThan(int years = 50)
+    {
+        if ((DateTime.Now.Year - year) > years)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public string GetFormattedInfo(string format = "short")
+    {
+
+        string PrintShortInfo()
+        {
+            return $"book: {title}, author: {author}";
+        }
+
+        if (format == "short")
+        {
+            return PrintShortInfo();
+        }
+
+        return GetInfo();
+
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+Book book1 = new Book("Война и мир", "Л. Толстой", 1869, 1225);
+Book book2 = new Book("Мастер и Маргарита", "М. Булгаков", 1967, 480);
+Book book3 = new Book("1984", "Дж. Оруэлл", 1949, 328);
+
+Console.WriteLine(book1.GetInfo());
