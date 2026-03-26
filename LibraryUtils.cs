e0cca003f425bb17d9d104bd4d73b4f1e6a63d68
Vladimir <vladimir.oleynyk057@gmail.com>
2026-03-26 08:15:57 +0300

    finished static class

--- LibraryUtils.cs
+++ LibraryUtils.cs
@@ -1,15 +1,16 @@
 
 
 using System.IO.Pipelines;
+using System.Security.Authentication.ExtendedProtection;
 
 static class LibraryUtils
 {
-    static public void PrintSeparator(char symbol = '-', int count = 40)
+    public static void PrintSeparator(char symbol = '-', int count = 40)
     {
         Console.WriteLine(new string(symbol, count));
     }
 
-    static public string FormatBookLists(Book[] books)
+    public static string FormatBookLists(Book[] books)
     {
         string res = "";
         int counter = 1;
@@ -21,7 +22,24 @@ static class LibraryUtils
             counter++;
         }
         return res;
+    }
 
+    public static Book FindOldest(Book[] books)
+    {
+        if (books.Length == 0)
+        {
+            return null;
+        }
+
+        Book res = books[0];
+        foreach (var book in books)
+        {
+            if (res.Year > book.Year)
+            {
+                res = book;
+            }
+        }
+        return res;
     }
 
 }
