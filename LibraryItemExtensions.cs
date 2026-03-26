2bc77f8b94c9cfd0239f666a498a42085441d967
vladimir <vladimir.oleynyk057@gmail.com>
2026-03-26 16:22:58 +0300

    add new task

---
+++ LibraryItemExtensions.cs
@@ -0,0 +1,38 @@
+
+public static class LibraryItemExtensions
+{
+    public static bool IsNew(this LibraryItem item, int year = 5)
+    {
+        if (DateTime.Now.Year - item.Year < year)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static string ToCsvLine(this LibraryItem item)
+    {
+        if (item is Book book)
+        {
+            return $"{item.GetType().Name};{book.Title};{book.Year};{book.Author}, {book.PageCount} стр.";
+        }
+        else if (item is Magazine magazine)
+        {
+            return $"{item.GetType().Name};{magazine.Title};{magazine.Year};{magazine.Publisher}, №{magazine.IssueNumber}";
+        }
+        else
+        {
+            throw new ArgumentException("don't know the item");
+        }
+    }
+    public static void PrintCard(this LibraryItem item)
+    {
+        string content = item.GetCardInfo();
+        int width = content.Length + 2;
+        
+        Console.WriteLine($"╔{new string('═', width)}╗");
+        Console.WriteLine($"║ {content} ║");
+        Console.WriteLine($"╚{new string('═', width)}╝");
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -8,13 +8,16 @@ LibraryItem[] items =
     new Magazine("Вокруг света", 2023, 5, "Издательский дом")
 };
 
-Console.WriteLine("=== Демонстрация полиморфизма ===\n");
+Console.WriteLine("=== Проверка методов расширения ===\n");
 
 foreach (var item in items)
 {
     Console.WriteLine($"--- {item.GetType().Name} ---");
-    Console.WriteLine($"GetInfo(): {item.GetInfo()}");
-    Console.WriteLine($"GetCardInfo(): {item.GetCardInfo()}");
-    Console.WriteLine($"Description: {item.Description}");
+    Console.WriteLine($"Название: {item.Title}");
+    Console.WriteLine($"Новое (5 лет): {item.IsNew()}");
+    Console.WriteLine($"Новое (10 лет): {item.IsNew(10)}");
+    Console.WriteLine($"CSV: {item.ToCsvLine()}");
+    Console.WriteLine("Каталожная карточка:");
+    item.PrintCard();
     Console.WriteLine();
 }
